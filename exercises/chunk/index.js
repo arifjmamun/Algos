8f0d133959c6c587cb64a819d829259111a38b04
Md. Ariful Islam <arifjmamun24@gmail.com>
2022-10-15 23:51:12 +0600

    Solution: Added alternative approach to solve chunk problem

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,20 +8,51 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
+/**
+ * 
+ * @param {any[]} array 
+ * @param {number} size
+ * @returns {any[][]}
+ */
 function chunk(array, size) {
   const chunked = [];
+  let index = 0;
 
-  for (let item of array) {
-    const lastChunk = chunked[chunked.length - 1];
-
-    if (!lastChunk || lastChunk.length === size) {
-      chunked.push([item]);
-    } else {
-      lastChunk.push(item);
-    }
+  while(index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
   }
 
   return chunked;
 }
 
 module.exports = chunk;
+
+
+// // Solution 1
+// function chunk(array, size) {
+//   const chunked = [];
+
+//   for (let item of array) {
+//     const lastChunk = chunked[chunked.length - 1];
+
+//     if (!lastChunk || lastChunk.length === size) {
+//       chunked.push([item]);
+//     } else {
+//       lastChunk.push(item);
+//     }
+//   }
+
+//   return chunked;
+// }
+
+// // Solution 2
+// function chunk(array, size) {
+//   const chunked = [];
+
+//   for (let i = 0; i < array.length; i += size) {
+//     chunked.push(array.slice(i, i + size));
+//   }
+
+//   return chunked;
+// }
